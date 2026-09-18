37165b2e44728ac395dba26f03c3bc7819281b13
Gustavo Augusto Oliveira Vitor <gustavovitorau@gmail.com>
2026-09-17 22:12:20 -0300

    Graças a Deus consegui arrumar o erro de compilação da configuração inicial, agora posso seguir com o projeto corretamente.. etapa Olá mundo no get da api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import express, {Request, Response} from "express";
 
 const app = express();
 
+app.get("/", (req: Request, res: Response)=>{
+    res.send("Olá, mundo!");
+});
+
 app.listen(8080, ()=>{
-    console.log("Servidor iniciado na porta 8080: https://localhost:8080")
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080")
 });
